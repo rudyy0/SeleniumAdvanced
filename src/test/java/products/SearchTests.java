11a23d81030b66f65e188d13002ee64d5e966ab4
Rafal <rafal.rudy94@gmail.com>
2022-06-11 12:49:24 +0200

    ok

--- src/test/java/products/SearchTests.java
+++ src/test/java/products/SearchTests.java
@@ -2,6 +2,7 @@ package products;
 
 import base.Pages;
 import org.assertj.core.api.Assertions;
+import org.assertj.core.api.SoftAssertions;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -18,9 +19,25 @@ public class SearchTests extends Pages {
 
         Assertions.assertThat(searchResults).isNotEmpty();
 
+        SoftAssertions softAssertions = new SoftAssertions();
+
         for (String searchResult : searchResults){
-            Assertions.assertThat(searchResult).contains(productToSearch);
+            softAssertions.assertThat(searchResult).contains(productToSearch);
         }
 
+        softAssertions.assertAll();
+    }
+
+    @Test
+    public void shouldFindProductWithName2() {
+        String productToSearch = productsGridPage.getRandomProductNameFromHomePage();
+
+        topMenuPage.searchForProduct(productToSearch);
+
+        List<String> searchResults = productsGridPage.getProductNames();
+
+        Assertions.assertThat(searchResults).isNotEmpty();
+
+        //Assertions.assertThat(searchResults).elements().contains(productToSearch);
     }
 }
